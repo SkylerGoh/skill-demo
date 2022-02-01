f9937faecc2865ca0d1a0fbdee6b7b68fa09739e
skyler <sjgoh@ucsd.edu>
2022-01-31 20:52:18 -0800

    Update DemoTester.java
    Fixed the test to pass

--- DemoTester.java
+++ DemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class DemoTester {
     @Test
     public void dadJokeTest() {
-        assertEquals("Hi Hungry, I'm dad.", Demo.dadJoke("Mom"));
+        assertEquals("Hi Hungry, I'm dad.", Demo.dadJoke("Hungry"));
     }
 }
